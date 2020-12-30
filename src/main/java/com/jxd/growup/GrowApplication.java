8af51f2aeb5bf97d540b0d296ffa518af355242e
littleyang666 <31499909+littleyang666@users.noreply.github.com>
2020-12-30 16:05:02 +0800

    我们的第一个提交项目

---
+++ src/main/java/com/jxd/growup/GrowApplication.java
@@ -0,0 +1,13 @@
+package com.jxd.growup;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GrowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrowApplication.class);
+    }
+}
